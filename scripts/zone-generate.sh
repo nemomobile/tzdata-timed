314b015c5daffbb37c2ec718b877602a94ee02fe
Petri M. Gerdt <petri.gerdt@jollamobile.com>
2013-03-12 15:39:10 +0200

    Remove generation of ICU data files

--- scripts/zone-generate.sh
+++ scripts/zone-generate.sh
@@ -14,21 +14,11 @@ signatures=signatures
 md5sum=md5sum
 links=zone.link
 
-icu_input=/usr/share/icu/icuzones
-icu_output=zones-icu
-icu_zic=/usr/bin/zic-icu
-icu_tz2icu=/usr/bin/tz2icu
-icu_genrb=/usr/bin/genrb
-
 test -x $zic
 test -x $yearistype
 test -x $signature
 test -r $leapseconds
-test -r $icu_input
-test -x $icu_zic
-test -x $icu_tz2icu
-test -x $icu_genrb
-rm -rf $output $icu_output $signatures $md5sum && mkdir -p $output $icu_output
+rm -rf $output $signatures $md5sum && mkdir -p $output
 
 input="africa antarctica asia australasia europe northamerica southamerica"
 input="$input iso8601"
@@ -43,15 +33,6 @@ for i in $input ; do
   cat stderr | grep -v "time zone abbreviation differs from POSIX standard" >&2 || true
 done
 $zic -d $output -p America/New_York
-
-echo "Processing all zones again (for libicu)"
-$icu_zic -y $yearistype -d $icu_output -L /dev/null $input $icu_input 2> stderr
-$icu_zic -d $icu_output -p America/New_York
-cat stderr | grep -v "time zone abbreviation differs from POSIX standard" >&2 || true
-echo -n "Olson data version:" && cat tzdata_version
-$icu_tz2icu $icu_output ./zone.tab $(cat tzdata_version)
-echo "Compiling zoneinfo64.txt file to .res binary"
-$icu_genrb -k -q -i . -d . zoneinfo64.txt
 
 zones=$(
   for f in $(cd $output ; echo *) ; do
